df62860c28d7d6a2362678cfa9b41f85b9325313
iaminfinityiq <nguyenbaoan.hn2408@gmail.com>
2026-07-06 10:03:42 +0700

    first commit

---
+++ cmd_version.py
@@ -0,0 +1,9 @@
+from os import system
+
+def cls() -> None:
+    system("clear")
+
+def petc() -> None:
+    input("Nhấn enter để tiếp tục...")
+
+print("Chào mừng bạn đến với trò chơi Tiến lên Miền Nam mở rộng")
